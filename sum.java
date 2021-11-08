5832f92a99ce7d9bf950ec170c17a0adf356c43c
Dipu Pradhan <dipuofficial116@gmail.com>
2021-11-08 06:37:05 -0500

    changed number on sum.java

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args){
         int a=5;
-        int b=6;
+        int b=666;
         
         System.out.println("the sum is"+(a+b));
         }
